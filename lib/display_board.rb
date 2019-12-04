ebad6396918a00d5db14bcc2bf694ab9dffd5564
Patrick Colina <trick.co@yahoo.com>
2019-12-04 06:11:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,16 +2,22 @@ title = "A Tic Tac Toe Board"
 
 puts title.upcase
 
-def display_board
-  row = "   |   |   "
-  border = "-----------"
-  puts row
-  puts border
-  puts row
-  puts border
-  puts row
+def infinity_tic_tac(x)
+  def display_board
+    row = "   |   |   "
+    border = "-----------"
+    puts row
+    puts border
+    puts row
+    puts border
+    puts row
+  end
+  x.times display_board
 end
 
-display_board
+infinity_tic_tac(5)
+
+
+
 
   # Define a method display_board that prints a 3x3 Tic Tac Toe Board
